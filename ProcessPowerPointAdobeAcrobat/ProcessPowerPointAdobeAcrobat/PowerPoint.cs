f082f5acf13bb78ea6a2c53de8eefab083523f43
dombrz09 <dombrz2@st.amu.edu.pl>
2017-05-27 21:09:19 +0200

    JoinProcess
    JoinProcess

---
+++ ProcessPowerPointAdobeAcrobat/ProcessPowerPointAdobeAcrobat/PowerPoint.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessPowerPointAdobeAcrobat
+{
+    public partial class PowerPoint : Form
+    {
+        Process powProcess;
+        Process[] collectionOfProcessP;
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        public PowerPoint()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            collectionOfProcessP = Process.GetProcessesByName("POWERPNT");
+           
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess = collectionOfProcessP[0];
+
+
+            }
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{Left}");
+            }
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{Right}");
+            }
+        }
+
+        private void zoomin_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("^{ADD}");
+            }
+        }
+
+        private void zoomout_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("^{SUBTRACT}");
+            }
+        }
+
+        private void pen_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("^{p}");
+            }
+
+        }
+
+        private void rubber_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("^{e}");
+            }
+
+        }
+
+        private void pointer_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("^{a}");
+            }
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{F5}");
+            }
+
+        }
+
+        private void end_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{ESC}");
+            }
+
+        }
+
+        private void stopstart_Click(object sender, EventArgs e)
+        {
+            if (collectionOfProcessP.Length >= 1)
+            {
+                powProcess.WaitForInputIdle();
+                IntPtr handle = powProcess.MainWindowHandle;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("{s}");
+            }
+
+        }
+
+        private void menu_Click(object sender, EventArgs e)
+        {
+            Start form1 = new Start();
+
+            this.Hide();
+
+            form1.Show();
+        }
+    }
+}
---
+++ ProcessPowerPointAdobeAcrobat/ProcessPowerPointAdobeAcrobat/Start.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ProcessPowerPointAdobeAcrobat
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.powerpoint = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // powerpoint
+            // 
+            this.powerpoint.Location = new System.Drawing.Point(35, 38);
+            this.powerpoint.Name = "powerpoint";
+            this.powerpoint.Size = new System.Drawing.Size(159, 42);
+            this.powerpoint.TabIndex = 0;
+            this.powerpoint.Text = "PowerPoint";
+            this.powerpoint.UseVisualStyleBackColor = true;
+            this.powerpoint.Click += new System.EventHandler(this.POWERPOINT_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "AdobeReader";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ADOBEREADER_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 141);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.powerpoint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button powerpoint;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
